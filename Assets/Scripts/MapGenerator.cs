7ec3f51e7bd2f47e2b955fd73a99b36cab4faa86
Peter <37542469+ppak10@users.noreply.github.com>
2018-07-18 15:06:21 -0400

    Episode 10 Commit
    Commit allows for meshes of different chunks to integrate with each other more optimally. There still seems to be a bug where the chunks do not render endlessly when moving the viewer

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -10,6 +10,8 @@ public class MapGenerator : MonoBehaviour {
 	public enum DrawMode {NoiseMap, ColorMap, Mesh};
 	public DrawMode drawMode;
 
+	public Noise.NormalizeMode normalizeMode;
+
 	public const int mapChunkSize = 241; // mesh is actually 240 by 240
 	[Range(0,6)]
 	public int editorPreviewLOD;
@@ -98,15 +100,16 @@ void Update() {
 	}
 }
 MapData GenerateMapData(Vector2 center) {
-		float[,] noiseMap = Noise.GenerateNoiseMap (mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, center+offset);
+		float[,] noiseMap = Noise.GenerateNoiseMap (mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, center+offset, normalizeMode);
 
 		Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
 		for (int y = 0; y < mapChunkSize; y++){
 			for (int x = 0; x < mapChunkSize; x++){
 				float currentHeight = noiseMap [x, y];
 				for (int i = 0; i < regions.Length; i++){
-					if (currentHeight <= regions [i].height){
+					if (currentHeight >= regions [i].height){
 						colorMap [y * mapChunkSize + x] = regions [i].color;
+					} else {
 						break;
 					}
 				}
