2aaffc0269e6c36c9196d83912effc6176170e24
Peter <37542469+ppak10@users.noreply.github.com>
2018-07-18 16:26:43 -0400

    Falloff Commit
    Commit allows for generation of island style mesh with falloff noisemap edits

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 public class MapGenerator : MonoBehaviour {
 
 
-	public enum DrawMode {NoiseMap, ColorMap, Mesh};
+	public enum DrawMode {NoiseMap, ColorMap, Mesh, FalloffMap};
 	public DrawMode drawMode;
 
 	public Noise.NormalizeMode normalizeMode;
@@ -26,14 +26,22 @@ public class MapGenerator : MonoBehaviour {
 	public int seed;
 	public Vector2 offset;
 
+	public bool useFalloff;
+
 	public float meshHeightMultiplier;
 	public AnimationCurve meshHeightCurve;
 	public bool autoUpdate;
 
 	public TerrainType[] regions;
 
+	float[,] falloffMap;
+
 	Queue<MapThreadInfo<MapData>> mapDataThreadInfoQueue = new Queue<MapThreadInfo<MapData>>();
 	Queue<MapThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>>();
+
+	void Awake() {
+		falloffMap = FalloffGenerator.GenerateFalloffMap(mapChunkSize);
+	}
 
 	public void DrawMapInEditor() {
 		MapData mapData = GenerateMapData (Vector2.zero);
@@ -44,6 +52,8 @@ public class MapGenerator : MonoBehaviour {
 			display.DrawTexture (TextureGenerator.TextureFromColorMap(mapData.colorMap, mapChunkSize, mapChunkSize));
 		} else if (drawMode == DrawMode.Mesh){
 			display.DrawMesh(MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve, editorPreviewLOD), TextureGenerator.TextureFromColorMap (mapData.colorMap, mapChunkSize, mapChunkSize));
+		} else if (drawMode == DrawMode.FalloffMap){
+			display.DrawTexture(TextureGenerator.TextureFromHeightMap(FalloffGenerator.GenerateFalloffMap(mapChunkSize)));
 		}
 
 	}
@@ -105,6 +115,9 @@ MapData GenerateMapData(Vector2 center) {
 		Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
 		for (int y = 0; y < mapChunkSize; y++){
 			for (int x = 0; x < mapChunkSize; x++){
+				if (useFalloff) {
+					noiseMap[x,y] = Mathf.Clamp01(noiseMap[x,y] - falloffMap [x,y]);
+				}
 				float currentHeight = noiseMap [x, y];
 				for (int i = 0; i < regions.Length; i++){
 					if (currentHeight >= regions [i].height){
@@ -126,6 +139,8 @@ MapData GenerateMapData(Vector2 center) {
 		if (octaves < 0) {
 			octaves = 0;
 		}
+
+		falloffMap = FalloffGenerator.GenerateFalloffMap(mapChunkSize);
 	}
 
 	struct MapThreadInfo<T> {
