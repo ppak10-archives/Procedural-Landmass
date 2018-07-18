e0e9e8dbac6cccbce14d4ed24fb56697c7d45db3
Peter <37542469+ppak10@users.noreply.github.com>
2018-07-18 17:27:35 -0400

    Seamless Commit
    Commit allows for light of textures and normals to be integrated with the mesh for seamless chunk rendering

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -12,7 +12,7 @@ public class MapGenerator : MonoBehaviour {
 
 	public Noise.NormalizeMode normalizeMode;
 
-	public const int mapChunkSize = 241; // mesh is actually 240 by 240
+	public const int mapChunkSize = 239; // mesh is actually 240 by 240
 	[Range(0,6)]
 	public int editorPreviewLOD;
 	public float noiseScale;
@@ -110,7 +110,7 @@ void Update() {
 	}
 }
 MapData GenerateMapData(Vector2 center) {
-		float[,] noiseMap = Noise.GenerateNoiseMap (mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, center+offset, normalizeMode);
+		float[,] noiseMap = Noise.GenerateNoiseMap (mapChunkSize + 2, mapChunkSize + 2, seed, noiseScale, octaves, persistance, lacunarity, center+offset, normalizeMode);
 
 		Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
 		for (int y = 0; y < mapChunkSize; y++){
